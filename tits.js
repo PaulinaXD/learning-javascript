387f3a7a86fa08d60a89ee855797fcb9288786e6
PaulinaXD <nather95@gmail.com>
2022-02-11 23:56:47 +0000

    learned let

---
+++ tits.js
@@ -0,0 +1,25 @@
+// let
+let age = 27;
+console.log(age);
+age = 28;
+console.log(age)
+age = 28 * 5;
+console.log(age)
+age = 'fuck off';
+console.log(age)
+
+
+// const
+
+// ==========
+// Tyoes
+// ==========
+// int
+
+// string
+
+// array
+
+// object
+
+console.log('live cum bucket')
