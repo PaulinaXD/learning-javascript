4cb16b5b61bc55e8e267a7b5bbf93785bab07ca5
PaulinaXD <nather95@gmail.com>
2022-02-12 23:20:42 +0000

    learned const & some types

--- tits.js
+++ tits.js
@@ -10,13 +10,29 @@ console.log(age)
 
 
 // const
+const mikesAge = 27;
+console.log(mikesAge);
+// mikesAge = mikesAge + 1;
+// console.log(mikesAge);
 
 // ==========
-// Tyoes
+// Types
 // ==========
-// int
+// number, int or float 543565.65
+const myInt = 100;
+const myFloat = 3.8;
+console.log(myInt / myFloat)
+
 
 // string
+const myName = 'Paulina';
+console.log(myName)
+const mySecondName = 'Kazimierek';
+const myFullName = `my name is ${myName} ${mySecondName}`;
+console.log(myFullName)
+// bool
+
+// null and undefined
 
 // array
 
